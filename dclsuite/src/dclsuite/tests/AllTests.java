758b470348e511c2e3e09c943d5722c1a2a35038
Luis <luis@fortran.(none)>
2012-10-08 11:48:15 -0300

    Inserting similarity test cases

--- dclsuite/src/dclsuite/tests/AllTests.java
+++ dclsuite/src/dclsuite/tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CoefficientTests.class, ExampleDivergenceTestCase.class, ExampleAbsenceTestCase.class,
+@SuiteClasses({StatisticsTests01.class, StatisticsTests02.class, CoefficientTests.class, ExampleDivergenceTestCase.class, ExampleAbsenceTestCase.class,
 	A001TestCase.class, A002TestCase.class, A003TestCase.class, A004TestCase.class,
 	A005TestCase.class, A006TestCase.class, A007TestCase.class, A008TestCase.class,
 	A009TestCase.class, A010TestCase.class, A011TestCase.class, A012TestCase.class,
