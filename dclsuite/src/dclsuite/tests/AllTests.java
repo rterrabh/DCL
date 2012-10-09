790d7d598dbce67eedecf5f67d94bb9be2045c48
Luis <luis@fortran.(none)>
2012-10-09 11:52:50 -0300

    Finishing test cases for create dependence

--- dclsuite/src/dclsuite/tests/AllTests.java
+++ dclsuite/src/dclsuite/tests/AllTests.java
@@ -10,10 +10,17 @@ import org.junit.runners.Suite.SuiteClasses;
 	A005TestCase.class, A006TestCase.class, A007TestCase.class, A008TestCase.class,
 	A009TestCase.class, A010TestCase.class, A011TestCase.class, A012TestCase.class,
 	A013TestCase.class, A014TestCase.class, A015TestCase.class, A016TestCase.class,
+	
 	A101TestCase.class, A102TestCase.class, A103TestCase.class, A104TestCase.class, A105TestCase.class, A106TestCase.class, A107TestCase.class,
 	A108TestCase.class, A109TestCase.class, A110TestCase.class, A111TestCase.class, A112TestCase.class, A113TestCase.class, A114TestCase.class,
 	A115TestCase.class, A116TestCase.class, A117TestCase.class, A118TestCase.class, A119TestCase.class, A120TestCase.class, A121TestCase.class, 
-	A122TestCase.class, A123TestCase.class, A124TestCase.class, A125TestCase.class, A126TestCase.class, A127TestCase.class, A128TestCase.class
+	A122TestCase.class, A123TestCase.class, A124TestCase.class, A125TestCase.class, A126TestCase.class, A127TestCase.class, A128TestCase.class,
+	
+	A201TestCase.class, A202TestCase.class, A203TestCase.class, A204TestCase.class, A205TestCase.class, A206TestCase.class, A207TestCase.class, A208TestCase.class, A209TestCase.class, A210TestCase.class,
+	A211TestCase.class, A212TestCase.class, A213TestCase.class, A214TestCase.class, A215TestCase.class, A216TestCase.class, A217TestCase.class, A218TestCase.class, A219TestCase.class, A220TestCase.class, 
+	A221TestCase.class, A222TestCase.class, A223TestCase.class, A224TestCase.class, A225TestCase.class, A226TestCase.class, A227TestCase.class, A228TestCase.class, A229TestCase.class, A230TestCase.class, 
+	A231TestCase.class, A232TestCase.class, A233TestCase.class, A234TestCase.class, A235TestCase.class, A236TestCase.class, A237TestCase.class, A238TestCase.class, A239TestCase.class, A240TestCase.class
+	
 	})
 public class AllTests {
 
